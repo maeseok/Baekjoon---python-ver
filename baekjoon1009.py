ad2c12ea326fcaa38fb6d835fb994cbb86afe6e2
maeseok <yumbum37@daum.net>
2022-09-28 11:55:43 +0000

    0928

---
+++ baekjoon1009.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+for i in range(0,n): 
+    a,b = map(int,input().split())
+    a = a % 10
+    #1. 0인 경우
+    if(a==0):
+        print(10)
+    #2. 1가지 경우만 나오는 경우
+    elif(a==1 or a==5 or a==6):
+        print(a)
+    #3. 2가지 경우가 나오는 경우
+    elif(a==4 or a==9):
+        b= b % 2
+        if(b==1):
+            print(a)
+        else:
+            print((a*a)%10)
+    #4. 4가지 경우가 나오는 경우
+    else:
+        b= b%4
+        if(b==0):
+            print(a**4%10)
+        else:
+            print(a**b%10)
+
+
+
+
---
+++ baekjoon1032.py
@@ -0,0 +1,15 @@
+N = int(input())
+c=[]
+for i in range(N):
+    a = list(map(str,input()))
+    if(i==0):
+        b= a
+        c= b
+    else:
+        for j in range(0,len(b)):
+            if(a[j] == b[j]):
+                pass
+            else:
+                c[j]="?"
+#join으로 리스트 값을 한줄로 출력
+print(''.join(c))
