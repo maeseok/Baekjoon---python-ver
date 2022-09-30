0657cde0542b4cf90ac51617b7d2a0666c99f657
maeseok <yumbum37@daum.net>
2022-09-30 15:10:01 +0000

    0930

--- B5/baekjoon2738.py
+++ B5/baekjoon2738.py
@@ -1,11 +1,23 @@
 import sys
 
+a = []
+b = []
+#입력할 크기 입력받음
 n,m = map(int,input().split())
-for i in range(2*n):
-    if(i<n):
-        list=list(map(str,sys.stdin.readline().split()))
-    if(i>=n):
-        List =list(map(str,sys.stdin.readline().split()))
-print(list,List)
 
-#아직 못 풀음
+#앞 데이터를 입력받아 저장
+for i in range(n):
+    data = list(map(int,sys.stdin.readline().split()))
+    a.append(data)
+
+#뒷 데이터를 입력받아 저장
+for i in range(n):
+    data = list(map(int,sys.stdin.readline().split()))
+    b.append(data)
+
+for i in range(n):
+    for j in range(m):
+    	#값을 더해 출력, 구분할 때는 띄어쓰기를 사용
+        print(a[i][j]+b[i][j],end=" ")
+    #한 줄 출력 후 다음 행으로
+    print()
